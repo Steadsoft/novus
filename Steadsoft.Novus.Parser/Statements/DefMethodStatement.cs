7f1152d0950d1a4d146e33ef5c94894cdbb8dda1
Korporal <hugh_gleaves@hotmail.com>
2021-11-08 13:52:44 -0700

    Fix the statement dump code.

---
+++ Steadsoft.Novus.Parser/Statements/DefMethodStatement.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Steadsoft.Novus.Parser
+{
+    public class DefMethodStatement : DefStatement
+    {
+        public BlockStatement Body { get; private set; }
+
+        public List<Parameter> Parameters { get; private set; }
+        public DefMethodStatement(DefStatement Stmt) : base(Stmt.Line, Stmt.Col, Stmt.Name)
+        {
+            this.Parameters = new List<Parameter>();
+        }
+
+        public void AddParameter(Parameter Parameter)
+        {
+            Parameters.Add(Parameter);
+        }
+
+        public void AddBody(BlockStatement Stmt)
+        {
+            if (Stmt == null) throw new ArgumentNullException(nameof(Stmt));
+
+            Body = Stmt;
+            Body.Parent = this;
+        }
+
+
+        public override string Dump(int nesting)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"{Prepad(nesting)}Method: [{Name}] {ParametersText} {String.Join<NovusKeywords>(", ", Options.OrderBy(op => op.ToString()))}");
+
+            builder.Append(Body.Dump(nesting));
+
+            return builder.ToString();
+        }
+
+        private string ParametersText
+        {
+            get
+            {
+                StringBuilder builder = new StringBuilder();
+
+                builder.Append('(');
+
+                foreach (var p in Parameters)
+                {
+                    builder.Append($"{p.Name} {p.TypeName}, ");
+                }
+
+                return builder.ToString().Trim(' ').Trim(',') + ")";
+            }
+        }
+    }
+}
--- Steadsoft.Novus.Parser/Statements/DefStatement.cs
+++ Steadsoft.Novus.Parser/Statements/DefStatement.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Steadsoft.Novus.Parser
@@ -28,42 +27,6 @@ namespace Steadsoft.Novus.Parser
         public override string Dump(int nesting)
         {
             throw new NotImplementedException();
-        }
-    }
-
-    public class DefMethodStatement : DefStatement
-    {
-        public BlockStatement Body { get; private set; }
-
-        public List<Parameter> Parameters { get; private set; }
-        public DefMethodStatement(DefStatement Stmt) : base(Stmt.Line, Stmt.Col, Stmt.Name)
-        {
-            this.Parameters = new List<Parameter>();
-        }
-
-        public void AddParameter(Parameter Parameter)
-        {
-            Parameters.Add(Parameter);
-        }
-
-        public void AddBody(BlockStatement Stmt)
-        {
-            if (Stmt == null) throw new ArgumentNullException(nameof(Stmt));
-
-            Body = Stmt;
-            Body.Parent = this;
-        }
-
-
-        public override string Dump(int nesting)
-        {
-            StringBuilder builder = new StringBuilder();
-
-            builder.AppendLine($"{Prepad(nesting)}Method: [{Name}] {String.Join<NovusKeywords>(", ", Options.OrderBy(op => op.ToString()))}");
-
-            builder.Append(Body.Dump(nesting));
-
-            return builder.ToString();
         }
     }
 }
