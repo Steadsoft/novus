3e1a3e7a889e8453c5f0a211cc5692428865233a
Korporal <hugh_gleaves@hotmail.com>
2021-11-09 17:51:56 -0700

    add comments

--- Steadsoft.Novus.Parser/ListExtensions.cs
+++ Steadsoft.Novus.Parser/ListExtensions.cs
@@ -2,6 +2,13 @@
 {
     public static class ListExtensions
     {
+        /// <summary>
+        /// Returns true if any of the values supplied in 'Set' appear more than once in the list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="List"></param>
+        /// <param name="Set"></param>
+        /// <returns></returns>
         public static bool ContainsMoreThanOneOf<T>(this List<T> List, params T[] Set) //where T : class
         {
             int count = 0;
@@ -20,6 +27,14 @@
             return false;
         }
 
+        /// <summary>
+        /// Returns true (and sets the out Item) if only one of the items appears and it appears only once in the list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="List"></param>
+        /// <param name="Item"></param>
+        /// <param name="Set"></param>
+        /// <returns></returns>
         public static bool TryGetUnique<T>(this List<T> List, out T Item, params T[] Set) //where T : class
         {
             Item = default;
