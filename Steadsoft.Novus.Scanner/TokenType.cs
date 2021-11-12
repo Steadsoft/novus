1867e35d20d4b7d7da84f081512fba2cf742ed59
Korporal <hugh_gleaves@hotmail.com>
2021-11-12 11:45:21 -0700

    Create a basic operator enum

--- Steadsoft.Novus.Scanner/TokenType.cs
+++ Steadsoft.Novus.Scanner/TokenType.cs
@@ -28,8 +28,49 @@
         SemiColon,
         Minus,
         Plus,
+        Times,
         Call,
         GoesTo,
         Comma
+    }
+
+    public enum Operator
+    {
+        /// <summary>
+        /// ->
+        /// </summary>
+        PointsTo = TokenType.PointsTo,
+        /// <summary>
+        /// =>
+        /// </summary>
+        GoesTo = TokenType.GoesTo,
+        /// <summary>
+        /// =
+        /// </summary>
+        Equals = TokenType.Equals,
+        /// <summary>
+        /// ==
+        /// </summary>
+        Equality = TokenType.Equals,
+        /// <summary>
+        /// >>
+        /// </summary>
+        ShiftRight = TokenType.ShiftRight,
+        /// <summary>
+        /// <<
+        /// </summary>
+        ShiftLeft = TokenType.ShiftLeft,
+        /// <summary>
+        /// -
+        /// </summary>
+        Minus = TokenType.Minus,
+        /// <summary>
+        /// +
+        /// </summary>
+        Plus = TokenType.Plus,
+        /// <summary>
+        /// *
+        /// </summary>
+        Times = TokenType.Times
     }
 }
