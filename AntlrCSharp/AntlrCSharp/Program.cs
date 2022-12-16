4df5b2b3043ce7d4f2c537009b12c2edabe24513
Hugh <hugh_gleaves@hotmail.com>
2022-12-16 07:32:59 -0700

    Add simple console sand box app that leverages the Antlr generated files.

---
+++ AntlrCSharp/AntlrCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AntlrCSharp
+{
+    internal class Program
+    {
+        private static string text =
+            $@"call = 1234{Environment.NewLine}ball = 1234{Environment.NewLine}";
+
+        static void Main(string[] args)
+        {
+            TextReader source = File.OpenText(@"..\..\..\..\..\Antlr\test1.nr");
+
+            AntlrInputStream inputStream = new AntlrInputStream(source);
+            noresLexer noresLexer = new noresLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(noresLexer);
+            noresParser noresParser = new noresParser(commonTokenStream);
+
+            noresParser.BuildParseTree = true;
+
+            var tree = noresParser.prog();
+
+            var listener = new TestListener();
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+
+            walker.Walk(listener, tree);
+            
+
+            Console.WriteLine(tree.ToStringTree());
+        }
+
+    }
+
+    public class SourceReader : TextReader
+    {
+
+    }
+}
---
+++ AntlrCSharp/AntlrCSharp/testListener.cs
@@ -0,0 +1,19 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntlrCSharp
+{
+    public class TestListener : noresBaseListener
+    {
+        public override void EnterBasic_reference([NotNull] noresParser.Basic_referenceContext context)
+        {
+            base.EnterBasic_reference(context);
+        }
+    }
+}
